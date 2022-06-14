6d2acb2ad1358126cc0f684bf50788d947ccfa2a
jnwnlee <james99322@gmail.com>
2022-06-15 01:03:24 +0900

    add carriers

--- carrier.py
+++ carrier.py
@@ -15,6 +15,10 @@ class Carrier():
             y = self.make_square_osc(freqs[0], self.dur, self.sr)
         elif self.name == 'exp_sawtooth':
             y = self.make_exp_sawtooth_osc(freqs[0], freqs[1], self.dur, self.sr)
+        elif self.name == 'log_sawtooth':
+            y = self.make_log_sawtooth_osc(freqs[0], freqs[1], self.dur, self.sr)
+        elif self.name == 'pow_sawtooth':
+            y = self.make_pow_sawtooth_osc(freqs[0], freqs[1], self.dur, self.sr)
         else:
             f_path = f"./audio_resources/carrier/{self.name}.wav"
             if not os.path.exists(f_path):
@@ -63,7 +67,7 @@ class Carrier():
         return x  
     
     def make_exp_sawtooth_osc(self, f_start, f_stop, dur, sr):
-        freq = self.powspace(f_start, f_stop, dur)
+        freq = self.expspace(f_start, f_stop, dur)
 
         phase = 0
         x = np.zeros(dur)
@@ -78,6 +82,43 @@ class Carrier():
 
         return x  
     
-    def powspace(self, start: float, stop: float, num: int):
+    def make_log_sawtooth_osc(self, f_start, f_stop, dur, sr):
+        freq = self.logspace(f_start, f_stop, dur)
+
+        phase = 0
+        x = np.zeros(dur)
+
+        for n in range(len(x)):
+            phase_inc = 2/(sr/freq[n])
+            phase = phase + phase_inc
+            if (phase > 1):
+                phase = phase - 2
+
+            x[n] = phase
+
+        return x  
+    
+    def make_pow_sawtooth_osc(self, f_start, f_stop, dur, sr):
+        freq = f_start + (f_stop-f_start)*(np.linspace(0, 1, dur)**0.2)
+
+        phase = 0
+        x = np.zeros(dur)
+
+        for n in range(len(x)):
+            phase_inc = 2/(sr/freq[n])
+            phase = phase + phase_inc
+            if (phase > 1):
+                phase = phase - 2
+
+            x[n] = phase
+
+        return x
+    
+    
+    def expspace(self, start: float, stop: float, num: int):
         log_start, log_stop = np.log(start), np.log(stop)
         return np.exp(np.linspace(log_start, log_stop, num))
+    
+    def logspace(self, start: float, stop: float, num: int):
+        exp_start, exp_stop = np.exp(start), np.exp(stop)
+        return np.log(np.linspace(exp_start, exp_stop, num))
